0550db3410156042f64a742a90a325eec652101c
Dartegnian <root@dartegnian.com>
2021-11-12 07:05:22 +0800

    Write simple script for showing current date and time, handle clicks

---
+++ pc/arch-main-pc/config/rofi/metro/charms.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+theme_location=$XDG_CONFIG_HOME/rofi/metro/charms.rasi
+time=$(date +%H:%M)
+time_midday=$(date +%p)
+day_name=$(date +%A)
+month_day=$(date "+%B %d")
+
+time_date_info="$time\n$time_midday\n$day_name\n$month_day"
+
+echo -e $time_date_info | rofi -dmenu -window-title rofi-charms -theme $theme_location
+exit_code="$?"
+
+case "$exit_code" in
+	10)
+		coproc ( catfish  > /dev/null  2>&1 )
+		;;
+	11)
+		coproc ( firefox  > /dev/null  2>&1 )
+		;;
+	12)
+		coproc ( rofi -show drun  > /dev/null  2>&1 )
+		;;
+	13)
+		coproc ( alacritty  > /dev/null  2>&1 )
+		;;
+	14)
+		coproc ( thunar ~/  > /dev/null  2>&1 )
+		;;
+	*)
+		;;
+esac
