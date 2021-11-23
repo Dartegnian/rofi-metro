000baf8dc34572659afe86e75cffa7b34e79ad2f
Dartegnian <root@dartegnian.com>
2021-11-24 03:25:13 +0800

    Use variables in charms application shortcuts

--- charms.sh
+++ charms.sh
@@ -6,6 +6,10 @@ time_midday=$(date +%p)
 day_name=$(date +%A)
 month_day=$(date "+%B %d")
 
+search_tool="catfish"
+terminal="alacritty"
+file_manager="thunar"
+
 time_date_info="$time\n$time_midday\n$day_name\n$month_day"
 
 echo -e $time_date_info | rofi -dmenu -window-title rofi-charms -theme $theme_location
@@ -13,19 +17,20 @@ exit_code="$?"
 
 case "$exit_code" in
 	10)
-		coproc ( catfish  > /dev/null  2>&1 )
+		coproc ( $search_tool > /dev/null  2>&1 )
 		;;
 	11)
-		coproc ( firefox  > /dev/null  2>&1 )
+		# Hopefully xdg-open starts up your default browser
+		coproc ( xdg-open http:// > /dev/null  2>&1 )
 		;;
 	12)
-		coproc ( rofi -show drun  > /dev/null  2>&1 )
+		coproc ( rofi -show drun > /dev/null  2>&1 )
 		;;
 	13)
-		coproc ( alacritty  > /dev/null  2>&1 )
+		coproc ( $terminal > /dev/null  2>&1 )
 		;;
 	14)
-		coproc ( thunar ~/  > /dev/null  2>&1 )
+		coproc ( $file_manager ~/ > /dev/null  2>&1 )
 		;;
 	*)
 		;;
